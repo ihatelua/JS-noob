ccbe3f523b6c3b62e83bbd021b8fc8afe4192ea5
ihatelua <ponyosushi@gmal.com>
2021-11-10 23:56:02 +0900

    commit

---
+++ 01. object/01. Create object/lecture01.js
@@ -0,0 +1,19 @@
+function Car(model, speed, color){
+    this.model = model;
+    this.speed = speed;
+    this.color = color;
+    this.break = function(){
+        this.speed -= 10;
+    }
+    this.accel = function(){
+        this.speed += 10;
+    }
+}
+
+myCar = new Car("520d", 60, "red");
+
+document.write("모델 : " + myCar.model + " 속도 : " + myCar.speed + "<br />");
+myCar.break();
+document.write("모델 : " + myCar.model + " 속도 : " + myCar.speed + "<br />");
+myCar.accel();
+document.write("모델 : " + myCar.model + " 속도 : " + myCar.speed + "<br />");
