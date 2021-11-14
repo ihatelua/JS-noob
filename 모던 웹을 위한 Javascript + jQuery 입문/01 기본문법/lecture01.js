518d2e79571af73a265c81a128e57b5984dfecfa
ihatelua <ponyosushi@gmal.com>
2021-11-14 19:13:48 +0900

    01 기본문법 예제추가

--- 모던 웹을 위한 Javascript + jQuery 입문/01 기본문법/lecture01.js
+++ 모던 웹을 위한 Javascript + jQuery 입문/01 기본문법/lecture01.js
@@ -1,4 +1,38 @@
-var 나 = "자바";
-var 너 = "스크립트";
+    var input = prompt('var 키워드의 비동기 함수 문제\n' + 
+                    '1. 비동기 함수와 var 키워드를 사용할 때\n'  + 
+                    '2. 비동기 함수와 var 키워드를 사용할 때의 문제 해결\n'  + 
+                    '3. let 키워드를 사용한 문제 해결\n'  + 
+                        '번호를 입력해주세요');
 
-document.write("'나' 의 이름 : " + 나 + "<br /> '너' 의 이름 : " + 너 + "<br />");
+
+    switch (input){
+        case "1":
+            for(var i = 0;i < 3; i++){
+                // 1초(1000밀리 초) * i초 후에 i를 출력
+                setTimeout(() => {
+                    alert(i);
+                }, 1000 * i);
+            }
+            break;
+        case "2":
+            for(var i = 0;i < 3; i++){
+                // 1초(1000밀리 초) * i초 후에 i를 출력
+                ((i) => {
+                        setTimeout(() => {
+                            alert(i);
+                        }, 1000 * i);
+                })(i);
+            }
+            break;
+
+        case "3":
+            for(let i = 0;i < 3; i++) {
+                // 1초(1000밀리 초) * i초 후에 i를 출력
+                setTimeout(() => {
+                    alert(i);
+                }, 1000 * i);
+            }
+            break;
+        default:
+            alert("잘못입력했습니다.");
+    }
