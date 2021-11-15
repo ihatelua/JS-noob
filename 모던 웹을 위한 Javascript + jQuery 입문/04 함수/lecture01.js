a089f1ba33af304ef5e7831de5755d74b7a95e31
ihatelua <ponyosushi@gmal.com>
2021-11-16 00:28:59 +0900

    클로저함수 예제 커밋

--- 모던 웹을 위한 Javascript + jQuery 입문/04 함수/lecture01.js
+++ 모던 웹을 위한 Javascript + jQuery 입문/04 함수/lecture01.js
@@ -1,12 +1,15 @@
-function MakeAdder(x){
-    return function(y){
-        return x + y;
+ function makeAdder(x) {
+      var y = 1;
+      return function(z) {
+        y = 100;
+        return x + y + z;
+      };
     }
-}
 
-const add3 = MakeAdder(3); 
-console.log(add3(2));
+    var add5 = makeAdder(5);
+    var add10 = makeAdder(10);
+    //클로저에 x와 y의 환경이 저장됨
 
-const add10 = MakeAdder(10);
-console.log(add10(5));
-console.log(add3(1));
+    console.log(add5(2));  // 107 (x:5 + y:100 + z:2)
+    console.log(add10(2)); // 112 (x:10 + y:100 + z:2)
+    //함수 실행 시 클로저에 저장된 x, y값에 접근하여 값을 계산
---
+++ 모던 웹을 위한 Javascript + jQuery 입문/04 함수/lecture02.js
@@ -0,0 +1,16 @@
+ function makeAdder(x) {
+      var y = 1;
+      return function(z) {
+        y = 100;
+        return x + y + z;
+      };
+    }
+
+    var add5 = makeAdder(5)(2); 
+    var add10 = makeAdder(10)(2); 
+    //클로저에 x와 y의 환경이 저장됨
+    
+    console.log(add5);    // 107 (x:5 + y:100 + z:2)
+    console.log(add10);   // 112 (x:10 + y:100 + z:2)
+    //함수 실행 시 클로저에 저장된 x, y값에 접근하여 값을 계산
+    
